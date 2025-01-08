3305c2d0c41d400e8d0dcec2c843b89f01741404
UsamaBinQamar <usama@idevnerds.com>
2025-01-08 12:27:35 +0500

    update

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Intro from '@/components/intro'   
+
+const page = () => {
+  return (
+    <section className="py-24">
+    <div className="container max-w-3xl">
+     <Intro/>
+     </div>
+  </section>
+  )
+}
+
+export default page
+
+
