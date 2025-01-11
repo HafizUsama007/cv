13aadeb5eb2dc2ee4e8c70841357d98525482455
UsamaBinQamar <usamadevio@gmail.com>
2025-01-11 21:36:38 +0500

    update

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 const nextConfig = {
-  output: "export", // Required for static exports
-};
+  output: 'export',  // Required for static site generation
+  images: {
+    unoptimized: true, // Required for static export
+  },
+  basePath:   'cv',
+  assetPrefix:   'cv',
+}
 
 export default nextConfig;
+
+ 
